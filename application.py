496fb7797e4496fc5a5d00945527643f70f4cb19
DaminiASunkad <daminisunkad@gmail.com>
2025-12-22 14:33:45 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,15 @@
+"""
+This script runs the FlaskWebProject application.
+"""
+
+from os import environ
+from FlaskWebProject import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '8000'))
+    except ValueError:
+        PORT = 8000
+
+    app.run(host=HOST, port=PORT)
